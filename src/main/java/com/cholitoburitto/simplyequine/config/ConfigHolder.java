8dac92c8f5f8e9418654ce27d48c064c0ba0be79
Silverwolf11420 <treefrog11420@gmail.com>
2020-09-10 16:11:59 -0600

    Added jump poles Added shavings block layers Added shavings block layers Added shavings block layers Added shavings block layers Added shavings block layers Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Fixed shavings block Added jump poles Added jump poles Added names Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Fixed shavings block Fixed shavings block Fixed shavings block Fixed shavings block Fixed shavings block Fixed shavings block Fixed shavings block Fixed shavings block Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added rubber Added rubber extractor Fixed shavings block Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added jump poles Added rubber Added rubber extractor Fixed shavings block Fixed thatch block Added rubber Added rubber extractor

--- src/main/java/com/cholitoburitto/simplyequine/config/ConfigHolder.java
+++ src/main/java/com/cholitoburitto/simplyequine/config/ConfigHolder.java
@@ -4,7 +4,6 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 
 public final class ConfigHolder {
-
     public static final ForgeConfigSpec COMMON_SPEC;
     static final CommonConfig COMMON;
 
